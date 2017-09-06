2e7292eb5936dfb95cef11ec17694718fc44b234
Vivek Nair <31657422+viveknair1402@users.noreply.github.com>
2017-09-06 20:34:35 +0530

    Update class exercise 1.r

--- class exercise 1.r
+++ class exercise 1.r
@@ -1,1 +1,21 @@
 # R1
+install.packages("data.table")
+install.packages("datasets")
+prof <- women   #Copying the data frame women into another dataset so that the original fields are not afected
+View(prof)
+
+id <- 0
+for(i in 1:nrow(prof))
+{
+  if
+  (prof$height[i]>65.0 & prof$weight[i]<136.7)
+  {
+  id<- id+1
+ }
+} 
+print("Number of women satisfying the criteria are")
+print(id)
+
+#Rough Space
+prof$height
+nrow(prof)
